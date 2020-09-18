8a169ad3138a4373d628ed530144430f8bf56758
Bogdan <bkozlovsky@bk.ru>
2020-09-18 16:42:59 +0300

    add hash

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from uuid import uuid1
-
 from fastapi import FastAPI
+from passlib.hash import pbkdf2_sha256
 from pydantic import BaseModel, Field
 from tortoise.models import Model
 from tortoise import fields
@@ -11,14 +11,15 @@ from typing import List
 
 
 class User(Model):
-    id = fields.CharField(50, pk=True)
+    id = fields.IntField(pk=True)
     user_name = fields.CharField(50, unique=True)
-    password = fields.CharField(50)
+    password = fields.CharField(150)
     first_name = fields.CharField(50)
     last_name = fields.CharField(50)
     gender = fields.CharField(1)
     create_at = fields.CharField(50)
     status = fields.CharField(1)
+    salt = fields.BinaryField()
 
 
 UserList = pydantic_model_creator(User, name="User")
@@ -43,19 +44,26 @@ async def find_all_users():
 
 @app.post("/users", response_model=UserList)
 async def register_user(user: UserEntry):
-    gTD = str(uuid1())
-    gDate = datetime.now()
+    salt = str(uuid1()).encode()
+    date = datetime.now()
     user = await User.create(
-        id=gTD,
         user_name=user.user_name,
-        password=user.password,
+        password=pbkdf2_sha256.using(salt=salt).hash(user.password),
         first_name=user.first_name,
         last_name=user.last_name,
         gender=user.gender,
-        create_at=gDate,
-        status="1"
+        create_at=date,
+        status="1",
+        salt=salt
     )
     return await UserList.from_tortoise_orm(user)
+
+
+@app.get("/login/{user_name}/{password}")
+async def login_user(user_name: str, password: str):
+    user_from_db = await User.get(user_name=user_name)
+    is_auth = user_from_db.password == pbkdf2_sha256.using(salt=user_from_db.salt).hash(password)
+    return {"is_auth": is_auth}
 
 
 register_tortoise(
